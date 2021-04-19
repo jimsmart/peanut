7d0f87aaf20b7840ecff1afbe2f3d72ca5f3cbbf
Jim Smart <jim@jimsmart.org>
2021-04-19 22:11:15 +0100

    Improved testing

--- multi_writer_test.go
+++ multi_writer_test.go
@@ -1,6 +1,8 @@
 package peanut_test
 
 import (
+	"errors"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -24,4 +26,38 @@ var _ = Describe("MultiWriter", func() {
 		// Did it actually write to both?
 		Expect(w1).To(Equal(w2))
 	})
+
+	It("should forward errors from Close", func() {
+
+		w1 := &peanut.MockWriter{}
+		w2 := &failWriter{}
+		w := peanut.MultiWriter(w1, w2)
+
+		err := w.Close()
+		Expect(err).NotTo(BeNil())
+	})
+
+	It("should forward errors from Cancel", func() {
+
+		w1 := &peanut.MockWriter{}
+		w2 := &failWriter{}
+		w := peanut.MultiWriter(w1, w2)
+
+		err := w.Cancel()
+		Expect(err).NotTo(BeNil())
+	})
 })
+
+type failWriter struct{}
+
+func (failWriter) Write(x interface{}) error {
+	return errors.New("fail")
+}
+
+func (failWriter) Close() error {
+	return errors.New("fail")
+}
+
+func (failWriter) Cancel() error {
+	return errors.New("fail")
+}
